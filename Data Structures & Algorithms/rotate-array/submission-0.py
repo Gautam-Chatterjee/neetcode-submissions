5e697a2879c1682779caa49babc6bdaf2074ce1c
Gautam Chatterjee <gchatterjee@umass.edu>
2026-07-07 00:06:46 -0400

    Add: rotate-array - submission-0

---
+++ Data Structures & Algorithms/rotate-array/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+        count = start = 0
+
+        while count < n:
+            current = start
+            prev = nums[start]
+            
+            while True:
+                next_idx = (current + k) % n
+                temp = nums[next_idx]
+                nums[next_idx] = prev
+                current = next_idx
+                prev = temp
+                count+=1
+
+                if start == current:
+                    break
+            
+            start+=1
