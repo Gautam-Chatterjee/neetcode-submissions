745ecb1337658c37b4cc19caf7dd609645c707b3
Gautam Chatterjee <gchatterjee@umass.edu>
2026-08-11 22:56:49 -0400

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        memo = {}
+
+        def dfs(n):
+            if n in memo.keys():
+                return memo[n]
+            if n < 0:
+                return 0
+            if n == 0:
+                return 1
+            
+            memo[n] = dfs(n-1) + dfs(n-2)
+            return memo[n]
+        
+        return dfs(n)
+       
+        
+
+
