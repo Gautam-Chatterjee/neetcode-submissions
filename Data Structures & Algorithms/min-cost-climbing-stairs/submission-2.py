461ad558a7ab98f1cb9240fac0cbf49a13c00d7f
Gautam Chatterjee <gchatterjee@umass.edu>
2026-08-11 23:10:49 -0400

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memo = {}
+
+        def dfs(i):
+            if i in memo.keys():
+                return memo[i]
+            if i >= len(cost):
+                return 0
+            
+            memo[i] = min(cost[i] + dfs(i+1), cost[i]+ dfs(i+2))
+            return memo[i]
+        
+        return min(dfs(0), dfs(1))
+
+            
+
+       
